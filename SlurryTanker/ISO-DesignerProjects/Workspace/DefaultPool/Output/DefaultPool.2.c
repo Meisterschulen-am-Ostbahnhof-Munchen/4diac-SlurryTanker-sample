40ce2a367f87a667607402ca30bff09b2986bfb6
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-04-01 14:50:26 +0200

    all 130

--- SlurryTanker/ISO-DesignerProjects/Workspace/DefaultPool/Output/DefaultPool.2.c
+++ SlurryTanker/ISO-DesignerProjects/Workspace/DefaultPool/Output/DefaultPool.2.c
@@ -6,32 +6,32 @@
 const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
 	     1,  // WorkingSet_0_Offset
 	    21,  // DataMask_1000_Offset
-	    41,  // SoftKeyMask_4000_Offset
-	    71,  // SoftKeyMask_4001_Offset
-	   101,  // SoftKey_5000_Offset
-	   114,  // SoftKey_5001_Offset
-	   127,  // SoftKey_5002_Offset
-	   140,  // SoftKey_5003_Offset
-	   153,  // SoftKey_5004_Offset
-	   166,  // SoftKey_5005_Offset
-	   179,  // SoftKey_5006_Offset
-	   192,  // SoftKey_5007_Offset
-	   205,  // SoftKey_5008_Offset
-	   218,  // SoftKey_5009_Offset
-	   231,  // SoftKey_5010_Offset
-	   244,  // SoftKey_5011_Offset
+	    41,  // SoftKeyMask_PAGE_1_Offset
+	    71,  // SoftKeyMask_PAGE_2_Offset
+	   101,  // SoftKey_Automatik_ein_Offset
+	   114,  // SoftKey_Dom_Offset
+	   127,  // SoftKey_Heckschieber_Offset
+	   140,  // SoftKey_Auseinanderklappen_Offset
+	   153,  // SoftKey_Verteiler_senken_Offset
+	   166,  // SoftKey_Knickdeichsel_senken_Offset
+	   179,  // SoftKey_Automatik_aus_Offset
+	   192,  // SoftKey_Schneidwerk_Offset
+	   205,  // SoftKey_Seitenschieber_Offset
+	   218,  // SoftKey_Zusammenklappen_Offset
+	   231,  // SoftKey_Verteiler_heben_Offset
+	   244,  // SoftKey_Knickdeichsel_heben_Offset
 	   257,  // SoftKey_5012_Offset
 	   264,  // SoftKey_5013_Offset
-	   271,  // SoftKey_5015_Offset
-	   284,  // SoftKey_5016_Offset
+	   271,  // SoftKey_Abscheider_Offset
+	   284,  // SoftKey_Licht_Offset
 	   297,  // SoftKey_5017_Offset
 	   304,  // SoftKey_5018_Offset
 	   311,  // SoftKey_5019_Offset
 	   318,  // SoftKey_5021_Offset
 	   325,  // SoftKey_5022_Offset
 	   332,  // SoftKey_5023_Offset
-	   339,  // Button_6000_Offset
-	   358,  // Button_6001_Offset
+	   339,  // PAGE_2_BTN_Offset
+	   358,  // PAGE_1_BTN_Offset
 	   377,  // page_2_MAIN_Offset
 	  1042,  // page_1_MAIN_Offset
 	  1707,  // workingset_pic_Offset
@@ -49,22 +49,22 @@ const unsigned long ISO_OP_MEMORY_CLASS isoOP_DefaultPool_Offset[] = {
 	  9318,  // Knickdeichsel_senken_Offset
 	  9947,  // lightbulb_Offset
 	 10476,  // trash_icon_Offset
-	 11141,  // page_1_AUX_Offset
-	 11806,  // page_2_AUX_Offset
+	 11141,  // page_1_for_AUX_Offset
+	 11806,  // page_2_for_AUX_Offset
 	 12471,  // AuxInput_Automatik_ein_Offset
-	 12483,  // AuxFunction2_31001_Offset
-	 12495,  // AuxFunction2_31002_Offset
+	 12483,  // AuxInput_Dom_Offset
+	 12495,  // AuxInput_Heckschieber_Offset
 	 12507,  // AuxInput_Auseinanderklappen_Offset
-	 12519,  // AuxFunction2_31004_Offset
-	 12531,  // AuxFunction2_31005_Offset
-	 12543,  // AuxFunction2_31006_Offset
-	 12555,  // AuxFunction2_31007_Offset
-	 12567,  // AuxFunction2_31008_Offset
-	 12579,  // AuxFunction2_31009_Offset
-	 12591,  // AuxFunction2_31010_Offset
-	 12603,  // AuxFunction2_31011_Offset
-	 12615,  // AuxFunction2_31015_Offset
-	 12627,  // AuxFunction2_31016_Offset
-	 12639,  // AuxFunction2_31024_Offset
-	 12651,  // AuxFunction2_31025_Offset
+	 12519,  // AuxInput_Verteiler_senken_Offset
+	 12531,  // AuxInput_Knickdeichsel_senken_Offset
+	 12543,  // AuxInput_Automatik_aus_Offset
+	 12555,  // AuxInput_Schneidwerk_Offset
+	 12567,  // AuxInput_Seitenschieber_Offset
+	 12579,  // AuxInput_Zusammenklappen_Offset
+	 12591,  // AuxInput_Verteiler_heben_Offset
+	 12603,  // AuxInput_Knickdeichsel_heben_Offset
+	 12615,  // AuxInput_Abscheider_Offset
+	 12627,  // AuxInput_Licht_Offset
+	 12639,  // PAGE_1_AUX_Offset
+	 12651,  // PAGE_2_AUX_Offset
 };  // isoOP_DefaultPool_Offset
