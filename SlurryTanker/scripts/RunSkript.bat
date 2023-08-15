10f41dda80ab2ef650ae3e31815694b0988b3241
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-08-15 09:44:50 +0200

    adopt

--- SlurryTanker/scripts/RunSkript.bat
+++ SlurryTanker/scripts/RunSkript.bat
@@ -4,8 +4,8 @@
 
 set PATH="C:\Espressif\python_env\idf5.0_py3.11_env\Scripts"
 
-del ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\DefaultPool.globalconsts
+del ..\4diacIDE-workspace\test\FBs\SlurryTanker\DefaultPool.globalconsts
 
-python PoolSkript.py --oldfile ..\ISO-DesignerProjects\Workspace\DefaultPool\Output\DefaultPool.iop.h --newfile ..\4diacIDE-workspace\test\FBs\Ventilsteuerung\DefaultPool
+python PoolSkript.py --oldfile ..\ISO-DesignerProjects\Workspace\DefaultPool\Output\DefaultPool.iop.h --newfile ..\4diacIDE-workspace\test\FBs\SlurryTanker\DefaultPool
 
 
